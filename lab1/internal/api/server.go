626df1196dade2b45c0d5b07e16b4bc40365763f
Konstantin <belikovkon@gmail.com>
2025-09-23 10:35:21 +0300

    lab1 third commit

--- lab1/internal/api/server.go
+++ lab1/internal/api/server.go
@@ -20,19 +20,14 @@ func StartServer() {
 	handler := handler.NewHandler(repo)
 
 	r := gin.Default()
-	// добавляем наш html/шаблон
+
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/static", "./resources")
-
-	// r.LoadHTMLGlob("../../templates/*")
-	// r.Static("../../static", "../../resources")
-	// слева название папки, в которую выгрузится наша статика
-	// справа путь к папке, в которой лежит статика
 
 	r.GET("/hello", handler.GetOrders)
 	r.GET("/order/:id", handler.GetOrder)
 	r.GET("/basket", handler.Basket)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run() 
 	log.Println("Server down")
 }
